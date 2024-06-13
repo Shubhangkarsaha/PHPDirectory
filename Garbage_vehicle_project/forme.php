d2bfce636e58fb93cb914aca498f6241fd0bc449
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-06-13 10:43:45 +0530

    Update forme.php

--- Garbage_vehicle_project/forme.php
+++ Garbage_vehicle_project/forme.php
@@ -1,182 +1,182 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
-<style type="text/css">
-<!--
-.style1 {
-	font-size: 24px;
-	font-weight: bold;
-}
-.style3 {font-size: 16px}
-.style5 {font-size: 22px; font-weight: bold; }
-.style6 {font-size: 18px}
--->
-</style>
-</head>
+// <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+// <html xmlns="http://www.w3.org/1999/xhtml">
+// <head>
+// <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+// <title>Untitled Document</title>
+// <style type="text/css">
+// <!--
+// .style1 {
+// 	font-size: 24px;
+// 	font-weight: bold;
+// }
+// .style3 {font-size: 16px}
+// .style5 {font-size: 22px; font-weight: bold; }
+// .style6 {font-size: 18px}
+// -->
+// </style>
+// </head>
 
-<body>
-<table width="90%" height="50%" border="0" align="center" cellpadding="1" cellspacing="1">
-  <tr>
-    <td colspan="4" align="center" valign="top"><span class="style5"><U>FORM - E</U></span><br />
-      (See rule 26)</td>
-  </tr>
-  <tr>
-    <td colspan="4">Claimants for family pension under rule 19 of the west Bengal Municipal (Employee's Death-cum-Retiring Benefits) Rules, 2003 Shall submit application to the pension sanctioning authority in the Form prescribed in part -1 below immediately after the death of the pensioner along with following documents :- </td>
-  </tr>
-  <tr>
-    <td width="1%">&nbsp;</td>
-    <td colspan="3">(1) Death certificate. </td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td colspan="3">(2) Two copies of recent passport size photograph duly attested by a Gazetted Officer. </td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td colspan="3">(3) Guardianship certificate where pension is admissible to a minor child /the child. </td>
-  </tr>
-  <tr>
-    <td colspan="4" align="center"><p class="style1"> PART 1</br>
-    <span class="style3">(See rule 26)</br></span><span class="style6">Form of application for family pension</span></p></td>
-  </tr>
-  <tr>
-    <td height="31" colspan="4">Application for a family pensionfor a family of Late / Shri / Smt _________________<br />
-    _____________________a________________(Designation) in the Siliguri Municipal Corporation. </td>
-  </tr>
-  <tr>
-    <td height="29" align="center">1.</td>
-    <td width="53%">Name of the applicant </td>
-    <td width="2%" align="center">:</td>
-    <td width="44%">_____________________________</td>
-  </tr>
-  <tr>
-    <td align="center">2.</td>
-    <td>Relationship to the employee / Pensioner </td>
-    <td align="center">:</td>
-    <td>_____________________________</td>
-  </tr>
-  <tr>
-    <td align="center">3.</td>
-    <td>Date of death of the employee/ Pensioner </td>
-    <td align="center">:</td>
-    <td>_____________________________</td>
-  </tr>
-  <tr>
-    <td align="center">4.</td>
-    <td>Date of retirement, if the deceased was a Pensioner </td>
-    <td align="center">:</td>
-    <td>_____________________________</td>
-  </tr>
-  <tr>
-    <td align="center">5.</td>
-    <td colspan="3">Name and age of surviving kindred of the deceased </td>
-  </tr>
-</table>
-<table width="90%" border="1" align="center" cellpadding="1" cellspacing="1">
-  <tr>
-    <th width="32%" height="33" scope="col">Relationship</th>
-    <th width="38%" scope="col">Name</th>
-    <th width="30%" scope="col">Date of Birth<br />
-    (By Christian era)</th>
-  </tr>
-  <tr>
-    <td align="left">(a). Widow / Widower </td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">(b). Sons </td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">(c). Unmarried Daughter </td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="left">(d). Parents </td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-</table>
-<table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
-  <tr>
-    <td width="3%" align="center" valign="top">6.</td>
-    <td colspan="3">Signature or left thumb impression (in the case of those who are lietrate enough to sign their name) ______________________ </td>
-  </tr>
-  <tr>
-    <td align="center" valign="top">7.</td>
-    <td colspan="3">Descriptive roll of Widow/Widower/guardian of the minor children/parents of late________________________________ </td>
-  </tr>
-  <tr>
-    <td align="center" valign="top">&nbsp;</td>
-    <td width="62%">(a) Date of birth (by Christian era) </td>
-    <td width="2%" align="center" valign="middle">:</td>
-    <td width="33%">__________________</td>
-  </tr>
-  <tr>
-    <td align="center" valign="top">&nbsp;</td>
-    <td>(b) Height </td>
-    <td align="center" valign="middle">:</td>
-    <td>__________________</td>
-  </tr>
-  <tr>
-    <td align="center" valign="top">&nbsp;</td>
-    <td>(c) Personal marks, if any , on hand or face </td>
-    <td align="center" valign="middle">:</td>
-    <td>__________________</td>
-  </tr>
-  <tr>
-    <td align="center" valign="top">&nbsp;</td>
-    <td>(d) Left hand thumb and finger impression </td>
-    <td align="center" valign="middle">&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-</table>
-<table width="90%" border="1" align="center" cellpadding="1" cellspacing="1">
-  <tr>
-    <td align="center">Small Finger </td>
-    <td align="center">Ring Finger </td>
-    <td align="center">Middle Finger </td>
-    <td align="center">Index Finger </td>
-    <td align="center">Thumb</td>
-  </tr>
-  <tr>
-    <td height="40">&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-</table>
-<table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
-  <tr>
-    <td align="center">Attested by :- </td>
-    <td align="center">Witness:-</td>
-  </tr>
-  <tr>
-    <td align="center">1) __________________ </td>
-    <td align="center">1) _______________ </td>
-  </tr>
-  <tr>
-    <td align="center">2) __________________ </td>
-    <td align="center">2) _______________ </td>
-  </tr>
-</table>
-<table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
-  <tr>
-    <td height="36" align="center">__________________________________________________________________________</td>
-  </tr>
-</table>
-<script>window.print();</script>
-<br />
-<table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
-  <tr>
-    <td align="center" valign="middle">Note :- The descriptive Roll (Column 7) and signature or left hand thumb and finger impression acompanying application for family pension should be in duplicate (in two separate sheets and attested by two Gazetted Officer/Pension Sanctioning Authority or person of respectability in the town, village or pargana in which theapplicant reside.) </td>
-  </tr>
-</table>
-</body>
-</html>
+// <body>
+// <table width="90%" height="50%" border="0" align="center" cellpadding="1" cellspacing="1">
+//   <tr>
+//     <td colspan="4" align="center" valign="top"><span class="style5"><U>FORM - E</U></span><br />
+//       (See rule 26)</td>
+//   </tr>
+//   <tr>
+//     <td colspan="4">Claimants for family pension under rule 19 of the west Bengal Municipal (Employee's Death-cum-Retiring Benefits) Rules, 2003 Shall submit application to the pension sanctioning authority in the Form prescribed in part -1 below immediately after the death of the pensioner along with following documents :- </td>
+//   </tr>
+//   <tr>
+//     <td width="1%">&nbsp;</td>
+//     <td colspan="3">(1) Death certificate. </td>
+//   </tr>
+//   <tr>
+//     <td>&nbsp;</td>
+//     <td colspan="3">(2) Two copies of recent passport size photograph duly attested by a Gazetted Officer. </td>
+//   </tr>
+//   <tr>
+//     <td>&nbsp;</td>
+//     <td colspan="3">(3) Guardianship certificate where pension is admissible to a minor child /the child. </td>
+//   </tr>
+//   <tr>
+//     <td colspan="4" align="center"><p class="style1"> PART 1</br>
+//     <span class="style3">(See rule 26)</br></span><span class="style6">Form of application for family pension</span></p></td>
+//   </tr>
+//   <tr>
+//     <td height="31" colspan="4">Application for a family pensionfor a family of Late / Shri / Smt _________________<br />
+//     _____________________a________________(Designation) in the Siliguri Municipal Corporation. </td>
+//   </tr>
+//   <tr>
+//     <td height="29" align="center">1.</td>
+//     <td width="53%">Name of the applicant </td>
+//     <td width="2%" align="center">:</td>
+//     <td width="44%">_____________________________</td>
+//   </tr>
+//   <tr>
+//     <td align="center">2.</td>
+//     <td>Relationship to the employee / Pensioner </td>
+//     <td align="center">:</td>
+//     <td>_____________________________</td>
+//   </tr>
+//   <tr>
+//     <td align="center">3.</td>
+//     <td>Date of death of the employee/ Pensioner </td>
+//     <td align="center">:</td>
+//     <td>_____________________________</td>
+//   </tr>
+//   <tr>
+//     <td align="center">4.</td>
+//     <td>Date of retirement, if the deceased was a Pensioner </td>
+//     <td align="center">:</td>
+//     <td>_____________________________</td>
+//   </tr>
+//   <tr>
+//     <td align="center">5.</td>
+//     <td colspan="3">Name and age of surviving kindred of the deceased </td>
+//   </tr>
+// </table>
+// <table width="90%" border="1" align="center" cellpadding="1" cellspacing="1">
+//   <tr>
+//     <th width="32%" height="33" scope="col">Relationship</th>
+//     <th width="38%" scope="col">Name</th>
+//     <th width="30%" scope="col">Date of Birth<br />
+//     (By Christian era)</th>
+//   </tr>
+//   <tr>
+//     <td align="left">(a). Widow / Widower </td>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+//   <tr>
+//     <td align="left">(b). Sons </td>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+//   <tr>
+//     <td align="left">(c). Unmarried Daughter </td>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+//   <tr>
+//     <td align="left">(d). Parents </td>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+// </table>
+// <table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
+//   <tr>
+//     <td width="3%" align="center" valign="top">6.</td>
+//     <td colspan="3">Signature or left thumb impression (in the case of those who are lietrate enough to sign their name) ______________________ </td>
+//   </tr>
+//   <tr>
+//     <td align="center" valign="top">7.</td>
+//     <td colspan="3">Descriptive roll of Widow/Widower/guardian of the minor children/parents of late________________________________ </td>
+//   </tr>
+//   <tr>
+//     <td align="center" valign="top">&nbsp;</td>
+//     <td width="62%">(a) Date of birth (by Christian era) </td>
+//     <td width="2%" align="center" valign="middle">:</td>
+//     <td width="33%">__________________</td>
+//   </tr>
+//   <tr>
+//     <td align="center" valign="top">&nbsp;</td>
+//     <td>(b) Height </td>
+//     <td align="center" valign="middle">:</td>
+//     <td>__________________</td>
+//   </tr>
+//   <tr>
+//     <td align="center" valign="top">&nbsp;</td>
+//     <td>(c) Personal marks, if any , on hand or face </td>
+//     <td align="center" valign="middle">:</td>
+//     <td>__________________</td>
+//   </tr>
+//   <tr>
+//     <td align="center" valign="top">&nbsp;</td>
+//     <td>(d) Left hand thumb and finger impression </td>
+//     <td align="center" valign="middle">&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+// </table>
+// <table width="90%" border="1" align="center" cellpadding="1" cellspacing="1">
+//   <tr>
+//     <td align="center">Small Finger </td>
+//     <td align="center">Ring Finger </td>
+//     <td align="center">Middle Finger </td>
+//     <td align="center">Index Finger </td>
+//     <td align="center">Thumb</td>
+//   </tr>
+//   <tr>
+//     <td height="40">&nbsp;</td>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+// </table>
+// <table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
+//   <tr>
+//     <td align="center">Attested by :- </td>
+//     <td align="center">Witness:-</td>
+//   </tr>
+//   <tr>
+//     <td align="center">1) __________________ </td>
+//     <td align="center">1) _______________ </td>
+//   </tr>
+//   <tr>
+//     <td align="center">2) __________________ </td>
+//     <td align="center">2) _______________ </td>
+//   </tr>
+// </table>
+// <table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
+//   <tr>
+//     <td height="36" align="center">__________________________________________________________________________</td>
+//   </tr>
+// </table>
+// <script>window.print();</script>
+// <br />
+// <table width="90%" border="0" align="center" cellpadding="1" cellspacing="1">
+//   <tr>
+//     <td align="center" valign="middle">Note :- The descriptive Roll (Column 7) and signature or left hand thumb and finger impression acompanying application for family pension should be in duplicate (in two separate sheets and attested by two Gazetted Officer/Pension Sanctioning Authority or person of respectability in the town, village or pargana in which theapplicant reside.) </td>
+//   </tr>
+// </table>
+// </body>
+// </html>
